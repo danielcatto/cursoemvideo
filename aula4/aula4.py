2e9bb37d61fdbd7679f5a4e59a7aca22d3e86747
daniel catto <danielcatto@hotmail.com>
2017-09-08 23:44:08 -0300

    aula4

---
+++ aula4/aula4.py
@@ -0,0 +1,11 @@
+nome = 'daniel'
+idade = 37
+peso = 79
+print('muito facil :D')
+print(nome, idade, peso)
+
+nome = input('qual é o seu nome:')
+idade = input('qual sua idade')
+peso = input('qual é o seu peso')
+
+print('olá {:4}\na sua idade é {}\ne seu peso é {:30}'.format(nome, idade, peso))
---
+++ aula4/ler_dia_mes_ano.py
@@ -0,0 +1,7 @@
+from datetime import date
+print("olá")
+nome = input("por favor informe\nseu nome")
+idade = int(input("idade"))
+ano = 2017 - idade
+
+print("olá {}, você nasceu em {}".format(nome,ano))
